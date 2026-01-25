a145e825b4b94ab6c2bef881dc46d687b32cdd9e
Burrito <burritoyangster@gmail.com>
2026-01-25 09:20:58 +0800

    Support Sonolus 1.1.0 (#41)

--- watch/src/engine/watchData/archetypes/Initialization.ts
+++ watch/src/engine/watchData/archetypes/Initialization.ts
@@ -122,10 +122,21 @@ export class Initialization extends Archetype {
                 background: false,
             })
 
-            ui.progress.set({
+            ui.progress.bar.set({
                 anchor: uiRect.rb,
                 pivot: { x: 0, y: 0 },
                 size: { x: uiRect.h, y: 0.15 * ui.configuration.progress.scale },
+                rotation: 90,
+                alpha: ui.configuration.progress.alpha,
+                horizontalAlign: HorizontalAlign.Center,
+                background: true,
+            })
+            ui.progress.graph.set({
+                anchor: uiRect.rb
+                    .sub(new Vec(gap, 0))
+                    .sub(new Vec(0.15 * ui.configuration.progress.scale, 0)),
+                pivot: { x: 0, y: 0 },
+                size: { x: uiRect.h, y: 0.3 * ui.configuration.progress.scale },
                 rotation: 90,
                 alpha: ui.configuration.progress.alpha,
                 horizontalAlign: HorizontalAlign.Center,
@@ -192,10 +203,21 @@ export class Initialization extends Archetype {
                 background: false,
             })
 
-            ui.progress.set({
+            ui.progress.bar.set({
                 anchor: uiRect.lb,
                 pivot: { x: 0, y: 0 },
                 size: { x: uiRect.w, y: 0.15 * ui.configuration.progress.scale },
+                rotation: 0,
+                alpha: ui.configuration.progress.alpha,
+                horizontalAlign: HorizontalAlign.Center,
+                background: true,
+            })
+            ui.progress.graph.set({
+                anchor: uiRect.lb
+                    .add(new Vec(0, gap))
+                    .add(new Vec(0, 0.15 * ui.configuration.progress.scale)),
+                pivot: { x: 0, y: 0 },
+                size: { x: uiRect.w, y: 0.3 * ui.configuration.progress.scale },
                 rotation: 0,
                 alpha: ui.configuration.progress.alpha,
                 horizontalAlign: HorizontalAlign.Center,
